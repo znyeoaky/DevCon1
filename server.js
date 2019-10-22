5c2a0b9f9d7f46e3e97ac8ce55a297d4d76cef83
BettingBat <55478832+znyeoaky@users.noreply.github.com>
2019-10-22 17:22:10 +1030

    commit

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ const PORT = process.env.PORT || 5000;
 //look for environment variable called port to use when we deploy to Heroku thats where its gonna get the port number.  locally run port 5000 to set default.
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-//take this variable pass to port then we can do a call back. /so if we want sth to happen once it connects, do consoleLog(back-ticks) to put template literal.
+//take this variable pass to port then we can do a call back.
+//so if we want sth to happen once it connects, do consoleLog(back-ticks) to put template literal.
